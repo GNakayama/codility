87a544b17b9566686f50f0e50705768d4c671b17
Guilherme <guilherme.nakayama.s@gmail.com>
2015-06-19 05:05:54 -0300

    - Lesson 5/Exercise4 Solution - Score 100% - Time Complexity O(N) - Memory complexity O(N)

---
+++ src/python/lesson5/exercise4.py
@@ -0,0 +1,40 @@
+def solution(S):
+    N = len(S)
+    symbol_stack = stack(N//2 + 1)    
+    
+    for k in xrange(N):
+        if S[k] == '{' or S[k] == '[' or S[k] == '(':
+            if symbol_stack.size == N//2 + 1:
+                return 0
+                
+            symbol_stack.push(S[k])
+        else:
+            if symbol_stack.size == 0:
+                return 0
+            
+            head = symbol_stack.pop()
+            
+            if (not (head == '{' and S[k] == '}')) and (not (head == '[' and S[k] == ']')) and (not (head == '(' and S[k] == ')')):
+                return 0
+    
+    if symbol_stack.size > 0:
+        return 0
+    
+    return 1
+    
+
+class stack(object):
+    def __init__(self, N):
+        self.size = 0
+        self._stack_ = [0] * N
+
+    def push(self, x):
+        self._stack_[self.size] = x
+        self.size += 1
+
+    def pop(self):
+        self.size -= 1
+        return self._stack_[self.size]
+
+    def head(self):
+        return self._stack_[self.size - 1]
