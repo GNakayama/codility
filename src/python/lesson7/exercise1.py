f472141a11dee85bf4f8efe684d3ff1c00dd4ba6
Guilherme <guilherme.nakayama.s@gmail.com>
2015-06-20 19:54:09 -0300

    - Lesson 7/Exercise1 Solution - Score 100% - Time Complexity O(N) - Memory complexity O(1)

--- src/python/lesson7/exercise1.py
+++ src/python/lesson7/exercise1.py
@@ -1,12 +1,19 @@
 def solution(A):
     return golden_max_slice(A)
-    
+
+
 def golden_max_slice(A):
-    max_ending = 0 
-    max_slice = 0
+    N = len(A)
+    max_ending = max_slice = A[0]
+
     
-    for a in A:
-        max_ending = max(0, max_ending + a)
-        max_slice = max(max_slice, max_ending)
+    for k in xrange(1, N):
+        if max_slice < 0:
+            if max_slice < A[k]:
+                max_slice = A[k]
+                max_ending = A[k]
+        else:        
+            max_ending = max(0, max_ending + A[k])
+            max_slice = max(max_slice, max_ending)
     
     return max_slice
