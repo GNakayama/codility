5101b7473415e9de5959bab54f825b6534cbd7e6
Guilherme <guilherme.nakayama.s@gmail.com>
2015-07-11 00:57:19 -0300

    Bug fix

--- src/python/lesson1/exercise1.py
+++ src/python/lesson1/exercise1.py
@@ -5,10 +5,10 @@
 def solution(A):
     N = len(A)
     total = sum(A)
-    result = total
-    left_sum = 0
+    left_sum = A[0]
+    result = abs(2 * left_sum - total)
 
-    for P in range(1, N):
+    for P in range(2, N):
         # For each loop calculates the sum A[0] + ... + A[P - 1]
         left_sum += A[P - 1]
         # Calculates the difference |A[0] + ... + A[P - 1] - (A[P] + ... + A[N - 1])|
