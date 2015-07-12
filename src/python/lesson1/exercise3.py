eedc63fa2dfc00d9652d8e8cc02b97b1097ac459
Guilherme <guilherme.nakayama.s@gmail.com>
2015-07-12 01:06:48 -0300

    - Adds problem Name and Link - Adds comments to code

--- src/python/lesson1/exercise3.py
+++ src/python/lesson1/exercise3.py
@@ -1,8 +1,14 @@
+# Name: PermMissingElem
+# Link: https://codility.com/demo/take-sample-test/perm_missing_elem/
+
+
 def solution(A):
-    n = len(A)    
-    total = ((n + 1)*(n + 2))/2
+    N = len(A)    
+    # Calcs the sum of all integers from 1 to N + 1
+    total = ((N + 1)*(N + 2))/2
     
-    for i in range(n):
+    for i in range(N):
+        # Removes all the elements of the array, from the sum, in the end this will gives us the missing element.
         total -= A[i]
     
     return total
