1502fce573c0d9642c0693f84e58acc9b73d2fe9
Guilherme <guilherme.nakayama.s@gmail.com>
2015-07-13 00:37:56 -0300

    - Adds Name and Link - Adds comments to code

--- src/python/lesson2/exercise2.py
+++ src/python/lesson2/exercise2.py
@@ -1,18 +1,20 @@
+# Name: PermCheck
+# Link: https://codility.com/demo/take-sample-test/perm_check/
+
+
 def solution(A):
-    n = len(A)
-    total = 0
-    count = [0]*(n + 1)
+    N = len(A)
+    # Counts appearance of elements of the sequence {1...N}
+    count = [0]*(N + 1)
         
-    for k in xrange(n):
-        if A[k] > n:
+    for k in xrange(N):
+        # If A has an element greater than N, A can't be a permutation from 1 to N
+        if A[k] > N:
             return 0;
+	# If it's the first time that A[k] appears count that, otherwise A can't be a permutation from 1 to N
         if count[A[k]] == 0:
-            total += 1
-            count[A[k]] += 1
+            count[A[k]] = 1
         else:
             return 0    
     
-    if total == n:
-        return 1
-    
-    return 0
+    return 1
