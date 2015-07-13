16074ac3d6e9b9eaf370dc914bf2af2d187b39cd
Guilherme <guilherme.nakayama.s@gmail.com>
2015-07-12 23:22:25 -0300

    - Adds Name and Link - Adds comments to code

--- src/python/lesson2/exercise1.py
+++ src/python/lesson2/exercise1.py
@@ -1,16 +1,22 @@
+# Name FrogRiverOne
+# Link https://codility.com/demo/take-sample-test/frog_river_one/
+
+
 def solution(X, A):
-    n = len(A)
+    N = len(A)
+    # Counts the appearance of leafes at every position from the 1 to X
     count = [0] * (X + 1)
     total = 0
         
-    for k in xrange(n):        
+    for k in xrange(N):        
+        # If it's the first time that the leaf appears at position A[k] adds one to total
         if count[A[k]] == 0:
             total += 1
-            
+
+            # If there is a leaf in every position from 1 to X, returns the current array index
             if total == X:
                 return k
-        
-        count[A[k]] = 1
-        
+        # Marks the position A[k] with a leaf
+        count[A[k]] = 1       
     
     return -1
