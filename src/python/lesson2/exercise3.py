b2dd122551effd84fb82e37beade9a9f423f7db5
Guilherme <guilherme.nakayama.s@gmail.com>
2015-07-13 05:56:56 -0300

    - Adds Name and Link - Adds comments to code

--- src/python/lesson2/exercise3.py
+++ src/python/lesson2/exercise3.py
@@ -1,15 +1,20 @@
+# Name: MissingInteger
+# Link: https://codility.com/demo/take-sample-test/missing_integer/
+
+
 def solution(A):
-    n = len(A)
-    count = [0]*(n + 1)
-    max = True
-        
-    for k in xrange(n):
-        if  (n + 1) > A[k] > 0:
+    N = len(A)
+    count = [0]*(N + 1)
+    
+    # Counts all elements of A tha belongs to sequence {1, ..., N}
+    for k in xrange(N):
+	if N >= A[k] > 0:
             count[A[k]] += 1
     
-    
-    for k in xrange(1, n + 1):
+    # Searches for the lesser integer with that not belongs to A 
+    for k in xrange(1, N + 1):
         if count[k] == 0:
             return k
     
-    return n + 1
+    # If A has all elements from 1 to N, N + 1 is the minimal integer
+    return N + 1
