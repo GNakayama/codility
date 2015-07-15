ab33ba628b526e96ed79f2e78c3ab52a95ef57e4
Guilherme <guilherme.nakayama.s@gmail.com>
2015-07-15 05:30:49 -0300

    - Adds Name an Link - Adds comments to code - Refactoring

--- src/python/lesson15/exercise1.py
+++ src/python/lesson15/exercise1.py
@@ -1,26 +1,40 @@
+
+# Name: NumbertSolitarie
+# Link: https://codility.com/demo/take-sample-test/number_solitaire/
+
+
 def solution(A):
     N = len(A)
     
+    # If we have only two elements the result should be the sum of this elements
     if N < 2:
         return A[0] + A[1]
     
     result = [0] * 7
-        
+    result[0] = A[0]
+    # First iteration, sets the first values for the result array
     for dice in xrange(7):       
-        if dice == 0:
-            result[dice] = A[0]
-        elif dice < N:
+        # The first movement so A[0] plus the next house ranging from 1 to 6, 
+        # notice that, if we have less than 6 houses on our board we have to ignore the results
+        if dice < N:
             result[dice] = A[0] + A[dice]
     
+    # Greedy loop
     for k in xrange(1, N):
-        for dice in xrange(7):
-            if dice == 0:
-                result[dice] = result[1]
-            elif dice == 6 and k + dice < N:
-                result[dice] = result[0] + A[k + dice] 
-            elif k + dice < N:
+	# First set the maximum result that got us at index k
+        result[0] = result[1]
+	# Calcs the next five (from 1 to 5) possible results departing from index k
+        for dice in xrange(1, 6):
+            if k + dice < N:
+  		# Keeps the greater result, to reach the house k + dice, 
+                # going trough house k may decrease the result, so we ignore it if that's it the case
                 result[dice] = max(result[dice + 1], result[0] + A[k + dice])
             else:
+		# We made all the necessary calculations
                 break
-                    
+         
+        # It's the first time we reached this house, so no need to do max() here          
+        if k + 6 < N:
+            result[6] = result[0] + A[k + 6]
+ 
     return result[0]
