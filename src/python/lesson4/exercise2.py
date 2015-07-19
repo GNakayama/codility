799f6e11677ab8808cbaaaaf4a2841e9325a9efd
Guilherme <guilherme.nakayama.s@gmail.com>
2015-07-19 09:57:55 -0300

    -Adds Link and Name -Adds comments to code

--- src/python/lesson4/exercise2.py
+++ src/python/lesson4/exercise2.py
@@ -1,19 +1,21 @@
+# Name: Triangle
+# Link: https://codility.com/demo/take-sample-test/triangle/
+
+
 def solution(A):
     N = len(A)
     A.sort()
     
-    if N > 3:        
-        for k in xrange(N - 2):       
-        
-            if test_triangle(A[k], A[k + 1], A[k + 2]):
-                return 1
-    else:
-        if N == 3:
-            if test_triangle(A[0], A[1], A[2]):
-                return 1
-    
-    return 0
+    # If A length is 3 this the only combination to check
+    if N == 3 and test_triangle(A[0], A[1], A[2]):
+        return 1
 
+    # Since Array is sorted, only have to look for each element once
+    for k in xrange(N - 1):
+        if test_triangle(A[k], A[k + 1], A[k + 2]):
+            return 1    
+
+    return 0
 
 def test_triangle(P, Q, R):
     return (P + Q) > R and (Q + R) > P and (R + P) > Q
