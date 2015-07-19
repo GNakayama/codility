6dc101ad6e387275a6e987c7b7fb4c23dc9f3b6b
Guilherme <guilherme.nakayama.s@gmail.com>
2015-07-18 23:14:41 -0300

    - Adds Name and Link - Adds comments to code

--- src/python/lesson5/exercise1.py
+++ src/python/lesson5/exercise1.py
@@ -1,17 +1,23 @@
+# Name: Nesting
+# Link: https://codility.com/demo/take-sample-test/nesting/
+
+
 def solution(S):
     N = len(S)
     size = 0
     
+    # Works as a stack where ( pushes an element, and ) pops one
     for k in xrange(N):
         if S[k] == '(':
             size += 1
         else:
             size -= 1
         
+	# If it try to pop an empty stack, error.
         if size < 0:
             return 0
     
-    
+    # If the stack is empty at the end of processing the string, string is properly nested
     if size == 0:
         return 1
     else:
